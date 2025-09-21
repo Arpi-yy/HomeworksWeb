4618312b447d7b58620b953d78539673d6b636d8
Arpi-yy <arpy.yeghiazaryan2007@gmail.com>
2025-09-21 23:59:12 +0400

    finished all homeworks for lesson 1, 2, 3 and 4

---
+++ lesson3.js
@@ -0,0 +1,59 @@
+
+//1
+// const prompt = require("prompt-sync")();
+// let number = Number(prompt("enter a number: "));
+// if(number>0){
+//     console.log(1);
+// }else if(number===0){
+//     console.log(0);
+// }else{
+//     console.log(-1);
+// }
+
+//2
+// let result;
+// if(a+b<4) result="Мало";
+// else result="Много";
+
+//3
+// for(let a = 2; a < 40; a+=2){
+//     console.log(a);
+// }
+
+//4
+// let num = 4;
+// for(let a = 0; a < 10; a++){
+//     console.log(String(a)+' * '+String(num)+' = '+String(a*num));
+// }
+
+//5
+// let sum = 0;
+// for(let a = 1; a < 14; a++){
+//     sum += a;
+// }
+// console.log(sum);
+
+//6
+// let sum = 0;
+// for(let a = 2; a < 40; a+=2){
+//     sum += a;
+// }
+// console.log(sum);
+
+//7
+// let sum = 0;
+// let num = 1;
+// while(num<40){
+//     sum += num;
+//     num += 2;
+// }
+// console.log(sum);
+
+//8
+// let multiplication = 1;
+// for(let a = 6; a < 60; a++){
+//     if(a%6===0){
+//         multiplication *= a;
+//     }
+// }
+// console.log(multiplication);
---
+++ lesson4.js
@@ -0,0 +1,41 @@
+
+//1
+// function odd_sum(num){
+//     let sum = 0;
+//     for(let a = 1; a < num; a++){
+//         if(a%2!==0){
+//             sum += a;
+//         }
+//     }
+//     return sum;
+// }
+
+//2
+// function is_to_five(num){
+//     if(num%5===0){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+//3
+// function are_even(num1,num2){
+//     if(num1%2===0&&num2%2===0){
+//         return num1+num2;
+//     }else{
+//         return num1*num2;
+//     }
+// }
+
+//4
+// function is_positive(num){
+//     if(num>0){
+//         console.log(String(num)+" is positive");
+//     }else if(num===0){
+//         console.log(String(num)+" is zero");
+//     }else{
+//         console.log(String(num)+" is negative");
+//     }
+// }
+
