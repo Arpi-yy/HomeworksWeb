759d18ec04c94ee3d0731b0548c740f245a23dce
Arpi-yy <arpy.yeghiazaryan2007@gmail.com>
2025-09-25 14:48:47 +0400

    I have corrected my webpage + I have done the 5th homework

---
+++ lesson5.js
@@ -0,0 +1,27 @@
+//1
+
+// let person = {"name" : "Arpi", "age" : 18, "city" : "Yerevan"};
+// person["isStudent"] = true;
+// delete person.city;
+// console.log(person.age !== undefined);
+// console.log(person);
+
+//2
+
+// function deletation(obj){
+//     for(let a in obj){
+//         if(obj[a] === undefined){
+//             delete obj[a]
+//         }
+//     }
+//     return obj;
+// }
+// obj1 = {
+// 	"key1" : "hello",
+// 	"key2" : undefined,
+// 	"key3" : 123,
+// 	"key4" : undefined
+// }
+// console.log(obj1);
+// deletation(obj1);
+// console.log(obj1);
